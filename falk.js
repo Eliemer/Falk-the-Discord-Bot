f66c5f161c5e73440e9302e2c3f599202225b97d
Chris Anger <Chris Anger>
2017-03-22 21:34:07 +1100

    Help command added, autorole untested

--- falk.js
+++ falk.js
@@ -18,6 +18,17 @@ bot.on("reconnecting", () => {
 	bot.user.setGame("Reconnecting");
 });
 
+// help command
+bot.on("message", msg => {
+    if(msg.content === prefix + "help"){
+        console.log(msg.author.username + " just asked for help");
+        msg.channel.sendCode("Markdown", "**Falk's help Menu** \n All commands begin with " + prefix +
+        "\n*myavatar*: Display your current avatar \n" +
+        "*ping*: Display your latency with falk \n" +
+        "*restart*: Restart Falk (for host only)\n");
+    }
+});
+
 // basic response to Falk being mentioned
 bot.on("message", msg => {
 	if(msg.isMentioned(bot.user.id)) {
