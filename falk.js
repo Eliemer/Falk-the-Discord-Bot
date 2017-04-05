7c34a5dcae28eb602a2bd63fb3dc86d1607cac29
Eliemer <eliemer619@gmail.com>
2017-04-05 13:53:00 -0400

    Major rewrite of source code, no more message listener duplicates

--- falk.js
+++ falk.js
@@ -1,6 +1,11 @@
 var Discord = require("discord.js");
 var bot = new Discord.Client();
-var token = "MjcyMDE0NDEwNTIyOTUxNjgw.C5lo4w.SpJBWNsC2JCJYjR19AHBLUQz9Sc";
+//var ytdl = require("ytdl-core");
+//var youtube = require("./youtube.js");
+//var ytAudioQueue = [];
+//var dispatcher = null;
+
+var token = "Mjc0MjY2NjgwMzkyMjg2MjE4.C8ahQQ.V7q3QHTdUcfSMVgj90UD1_gRW5Y";
 var prefix = "falk.";
 var idle = "with his prey";
 var ready = true;
@@ -19,77 +24,59 @@ bot.on("reconnecting", () => {
 	bot.user.setGame("Reconnecting");
 });
 
-// help command
+// message commands
 bot.on("message", msg => {
-    if(msg.content === prefix + "help"){
-        console.log(msg.author.username + " just asked for help");
-        msg.channel.send("```Markdown" + "\n\<\*\*Falk's Help Menu\*\* \n All commands begin with " + prefix + "\>" +
-        "\n\n\*myavatar\*: Display your current avatar \n" +
-        "\*ping\*: Display your latency with falk \n" +
-        "\*restart\*: Restart Falk (for host only)\n" + "```");
+    var msgParts = msg.content.split(" ");
+    var command = msgParts[0].toLowerCase();// first word of message
+    var parameters = msgParts.splice(1, msgParts.length);
+
+    console.log("command: " + command);
+    console.log("parameters: " + parameters);
+
+    switch (command) {
+        case prefix + "help":
+            console.log(msg.author.username + " just asked for help");
+            msg.channel.send("`Falk's Help Menu`\n\n" +
+            "All commands have the form of " + prefix + "[command]\n\n" +
+            "`myavatar`: Display your current Discord Avatar\n" +
+            "`ping`: Display Falk's latency\n" +
+            "`restart`: Restart Falk (for host only)\n" +
+            "`die`: Shutdown Falk (for host only)\n").catch(console.error);
+            break;
+        case prefix + "myavatar":
+            console.log(msg.author.username + " requested their avatar");
+            bot.user.setGame("with an avatar");
+            msg.reply("Here is you avatar - " + msg.author.avatarURL).catch(console.error);
+            bot.user.setGame(idle);
+            break;
+        case "buttsax":
+            console.log(msg.author.username + " initiated the call of the buttsax");
+            msg.channel.send(":peach::saxophone::notes::notes::notes::notes::notes::notes:");
+            break;
+        case prefix + "ping":
+            console.log("pong");
+            msg.reply("BORK! (" + bot.ping + "ms)").catch(console.error);
+            break;
+        case prefix + "die":
+            console.log("destroy");
+            bot.user.setGame("with fire");
+            msg.channel.send(":dog::wave:");
+            bot.destroy().catch(console.error);
+            break;
+        case prefix + "restart":
+            bot.user.setGame("with resurrection");
+            console.log("restart");
+            msg.channel.send(":wave:");
+            bot.destroy().catch(console.error);
+            bot.login(token).catch(console.error);
+            break;
+    }//end of switch command
+
+    if(msg.isMentioned(bot.user.id)){
+        console.log(msg.author.username + " just mentioned Falk");
+        msg.reply("**BORK!! BORK!! DO NOT MENTION THE WOLF!! BORK!! BORK!!**");
     }
-});
-
-// basic response to Falk being mentioned
-bot.on("message", msg => {
-	if(msg.isMentioned(bot.user.id)) {
-		console.log(msg.author.username + " just mentioned Falk");
-		msg.reply("**BORK!! BORK!! DO NOT MENTION THE WOLF!! BORK!! BORK!!**");
-	}
-});
-
-/* autorole plus console message
-bot.on("guildMemberAdd", member => {
-    console.log(member.user.username + "(" + member.user.id + ")" + " has joined");
-    member.addRole(newbRole);
-    member.channel.send(member.user.username + " has joined the server");
-  console.log ("New user: " + member.user.id + " has been processed");
-}); */
-
-// display your avatar
-bot.on("message", msg => {
-	if (msg.content === prefix + "myavatar") {
-		bot.user.setGame("with an avatar");
-		console.log(msg.author.username + " requested their avatar");
-		msg.reply("Here is your avatar - " + msg.author.avatarURL).catch(console.error);
-		bot.user.setGame(idle);
-	}
-});
-
-/* leave server console message
-bot.on("guildMemberRemove", member => {
-	console.log(member.user.username + "(" + member.user.id + ")" + " left the server");
-             msg.channel.send(member.user.username + " has left the server").catch(console.error);
-}); */
-
-// ping pong
-bot.on("message", msg => {
-	if (msg.content === prefix + "ping") {
-		console.log("pong");
-		msg.reply("BORK! (" + bot.ping + "ms)").catch(console.error);
-	}
-});
-
-// bot destroy command
-bot.on("message", msg => {
-	if (msg.content === prefix + "die") {
-		bot.user.setGame("with fire");
-		console.log("destroy");
-		msg.channel.send(":dog::wave:");
-		bot.destroy().catch(console.error);
-	}
-});
-
-// bot restart
-bot.on("message", msg => {
-	if (msg.content === prefix + "restart") {
-		bot.user.setGame("with resurrection");
-		console.log("restart");
-		msg.channel.send(":wave:");
-		bot.destroy().catch(console.error);
-		bot.login(token).catch(console.error);
-	}
-});
+});//end of msg listener
 
 // login to bot account
 console.log("Connecting...");
